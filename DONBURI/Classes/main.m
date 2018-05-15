a4a7581e6819418364bf939565e466533e063210
newmoon1102 <dinhchung.utc.k49@gmail.com>
2018-05-15 18:56:33 +0900

    Add files via upload

---
+++ DONBURI/Classes/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  DemoPepper
+//
+//  Created by OA-Promotion-Center on 2016/07/28.
+//  Copyright © 2016 OA-Promotion-Center. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
